8eb0d42f5bc0bb00f36cd87cb39a2ea593c5b827
siddik11803-IIITH <siddik.naraharisetti@students.iiit.ac.in>
2023-04-24 12:25:54 +0530

    preprocessing done

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
 from utils import *
 
 
-print(import_csv())
+df = import_csv()
+
+# Getting Gender and Age
+gender = df['gender']
+age = df['age']
+df = df.drop(['gender', 'age'], axis = 1)
+
+experience = df['experience']
+df['experience'] = return_exp(experience)
+
+
+df_experience = df[df['experience'] == 1].drop(['experience'], axis = 1)
+df_unexperience = df[df['experience'] == 0].drop(['experience'], axis = 1)
+
+dfs_experience = split_df(df_experience)
+dfs_unexperience = split_df(df_unexperience)
+
+mean_experience = np.array([j.mean().values for j in dfs_experience])
+mean_unexperience = np.array([j.mean().values for j in dfs_unexperience])
+
+
+bin_mean_exp = convert_bool(mean_experience)
+bin_mean_unexp = convert_bool(mean_unexperience)
--- utils.py
+++ utils.py
@@ -2,10 +2,53 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def create_dict():
+    dict_1 = dict()
+    for i in range(20):
+        if(i != 0):
+            dict_1["On a scale of 1-10 how warm is the song? (10 corresponds to very warm)."+str(i)] = "q"+str(i)+"_warm"
+            dict_1["On a scale of 1-10 how acoustic is the song? (10 corresponds to very acoustic)."+str(i)] = "q"+str(i)+"_acoustic"
+            dict_1["On a scale of 1-10 how colourful is the song? (10 corresponds to very colourful)."+str(i)] = "q"+str(i)+"_coloutful"
+            dict_1["On a scale of 1-10 how full is the song? (10 corresponds to very full)."+str(i)] = "q"+str(i)+"_full"
+            dict_1["On a scale of 1-10 how Energetic is the song? (10 corresponds to very Energetic)."+str(i)] = "q"+str(i)+"_full"
+        else:
+            dict_1["On a scale of 1-10 how warm is the song? (10 corresponds to very warm)"] = "q"+str(i)+"_warm"
+            dict_1["On a scale of 1-10 how acoustic is the song? (10 corresponds to very acoustic)"] = "q"+str(i)+"_acoustic"
+            dict_1["On a scale of 1-10 how colourful is the song? (10 corresponds to very colourful)"] = "q"+str(i)+"_colourful"
+            dict_1["On a scale of 1-10 how full is the song? (10 corresponds to very full)"] = "q"+str(i)+"_full"
+            dict_1["On a scale of 1-10 how Energetic is the song? (10 corresponds to very Energetic)"] = "q"+str(i)+"_energy"
+    return dict_1
+
 
 def import_csv():
     """
     Imports the csv file and returns a pandas dataframe
     """
     df = pd.read_csv('./data.csv')
+    df = df.drop(['Timestamp'], axis = 1)
+    df = df.rename(columns={"Enter the amount of experience you have in music (Instruments or Vocals)":"experience", "Please enter your age in years":"age", "Please select your gender":"gender"})
+    df = df.rename(columns=create_dict())
     return df
+
+def return_exp(experience):
+    list_1 = []
+    for i in range(len(experience)):
+        if(experience[i] == "No Experience"):
+            list_1 += [0]
+        else:
+            list_1 += [1]
+    return list_1
+
+def split_df(df):
+    dfs = np.array_split(df, len(df.columns)//5, axis = 1)
+    return dfs
+
+def convert_bool(mean_array):
+    for i in range(len(mean_array)):
+        for j in range(len(mean_array[i])):
+            if(mean_array[i][j] > 5):
+                mean_array[i][j] = 1
+            else:
+                mean_array[i][j] = 0
+    return mean_array
+
